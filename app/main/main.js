750c6956d5d018f00554ae1008f34d3fb6b48c29
Michael Callaghan <michael.d.callaghan@disney.com>
2016-05-11 10:22:44 -0400

    Updated routes

--- app/main/main.js
+++ app/main/main.js
@@ -10,11 +10,67 @@ angular.module('main', [
   // ROUTING with ui.router
   $urlRouterProvider.otherwise('/main');
   $stateProvider
-    // this state is placed in the <ion-nav-view> in the index.html
-    .state('main', {
-      url: '/main',
-      template: '<ion-view view-title="main"></ion-view>',
-      // templateUrl: 'main/templates/<someTemplate>.html',
-      // controller: 'SomeCtrl as ctrl'
-    });
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('home', {
+    url: '/main',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+
+  .state('tabsController.students', {
+    url: '/students',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/students.html',
+        controller: 'studentsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.classes', {
+    url: '/classes',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/classes.html',
+        controller: 'classesCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.class', {
+    url: '/class/:id',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/class.html',
+        controller: 'classCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.student', {
+    url: '/student/:id',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/student.html',
+        controller: 'studentCtrl'
+      }
+    }
+  })
+
+  .state('message', {
+    url: '/message/:id',
+    templateUrl: 'templates/message.html',
+    controller: 'messageCtrl'
+  })
+
+  .state('roster', {
+    url: '/roster/:id',
+    templateUrl: 'templates/roster.html',
+    controller: 'rosterCtrl'
+  });
 });
