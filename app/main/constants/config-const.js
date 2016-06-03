6979029251211f6afc813ea3579937573f3fbf9f
Michael Callaghan <michael.d.callaghan@disney.com>
2016-06-03 10:46:56 -0400

    Finished Module 3

--- app/main/constants/config-const.js
+++ app/main/constants/config-const.js
@@ -5,8 +5,8 @@ angular.module('main')
   // gulp environment: injects environment vars
   ENV: {
     /*inject-env*/
-    'SERVER_URL': 'https://DEVSERVER/api',
-    'SOME_OTHER_URL': '/proxy'
+    'CLASSES_URL': './main/assets/data/classes.json',
+    'STUDENTS_URL': './main/assets/data/students.json'
     /*endinject*/
   },
 
